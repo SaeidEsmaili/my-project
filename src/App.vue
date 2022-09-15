6e38677b7fe8dea2b8824f169d3f3d269f7b4ed7
saeidjooon <saeid.esmaili2013@gmail.com>
2022-09-15 22:05:12 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{title}}</h1>
+ 
+
+ <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Modal},
+ data(){
+  return{
+    title: 'test for webpage'
   }
+ },
+ methods: {
+  handleEvent(){
+    console.log('test');
+    console.log(this.$refs.name);
+    this.$refs.name.classList.add('test')
+  }
+ }
 }
 </script>
 
@@ -22,5 +36,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  color: blueviolet;
 }
 </style>
